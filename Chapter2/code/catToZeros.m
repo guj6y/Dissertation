ee840523f93b03f68763c7c79509068bdaee696b
Nic Kappler <nguj6y@gmail.com>
2018-03-14 18:51:09 -0500

    fixed a problem here

--- Chapter2/code/catToZeros.m
+++
@@ -1,7 +0,0 @@
-function [x] = catToZeros(x,y)
-%x_ = x;
-    [height,~] = size(x);
-    %x([zeros(height,1) diff((x==0)')']>0) = y;
-    %x(diff(([ones(height,1) x]==0)')'>0) = y;
-    x((sum(x>0,2))*height + (1:height)) = y;
-end
